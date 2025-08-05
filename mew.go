c4eaa1977a772e7050d87cb80b6f089a1beb2f91
PRABHAT1SHUKLA <prabhatshukla0849@gmail.com>
2025-08-05 19:39:38 +0530

    some thing

---
+++ mew.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variable declaration with type
+	var name string = "Alice"
+
+	// Type inference
+	age := 30
+
+	// Multiple variable declarations
+	var a, b, c int = 1, 2, 3
+
+	// Constants
+	const pi = 3.14159
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("a, b, c:", a, b, c)
+	fmt.Println("Pi:", pi)
+}
